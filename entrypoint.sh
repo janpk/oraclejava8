192b39d8c90a44df5d9889eaf5deec8156267ef2
Jan-Petter Krüger <jan.petter.kruger@evry.com>
2017-03-22 10:56:29 +0100

    Updated to use gosu

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# If GOSU_CHOWN environment variable set, recursively chown all specified directories 
+# to match the user:group set in GOSU_USER environment variable.
+if [ -n "$GOSU_CHOWN" ]; then
+  for DIR in $GOSU_CHOWN
+  do
+    chown -R $GOSU_USER $DIR
+  done
+fi  
+
+# If GOSU_USER environment variable set to something other than 0:0 (root:root),
+# become user:group set within and exec command passed in args
+if [ -n "$GOSU_USER" ] && [ "$GOSU_USER" != "0:0" ]; then
+  IFS=':' read -r -a uidgid <<< $GOSU_USER
+  groupmod -o -g "${uidgid[1]}" app || true
+  usermod -u "${uidgid[0]}" -g "${uidgid[1]}" app
+  exec gosu $GOSU_USER "$@"
+fi
+
+chown -R app:app /app
+
+# If GOSU_USER was 0:0 exec command passed in args without gosu (assume already root)
+exec "$@"
